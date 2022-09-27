064bae7b929cefefe6016ded315aba33c983c1fd
Cesar Preza <cpreza93@yahoo.com>
2022-09-26 19:38:36 -0700

    Adds quotes to select from

---
+++ script.js
@@ -0,0 +1,28 @@
+let quotes = [
+    {
+        quote: 'The way to get started is to quit talking and begin doing.',
+        source: 'Walt Disney'
+    },
+    {
+        quote: 'Life would be tragic if it weren\'\t funny.',
+        source: 'Stephen Hawking'
+    },
+    {
+        quote: 'Never let the fear of striking out keep you from playing the game.',
+        source: 'Babe Ruth'
+    },
+    {
+        quote: 'The big lesson in life, baby, is never be scared of anyone or anything.',
+        source: 'Frank Sinatra'
+    },
+    {
+        quote: 'Everything negative - pressure, challenges - is all an opportunity for me to rise.',
+        source: 'Kobe Bryant'
+    },    
+]
+
+
+
+
+
+
